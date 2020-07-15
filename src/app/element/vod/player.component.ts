e43f2af7a3752fcfae5c285461932312ac9bb382
jongwoo Yoo <dbwhddn10@gmail.com>
2020-07-15 22:14:06 +0900

    change video library from videojs to hls.js

--- src/app/element/vod/player.component.ts
+++ src/app/element/vod/player.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Vod } from 'src/app/model/vod';
+import Hls from 'hls.js';
 import * as _ from 'lodash';
 
 @Component({
@@ -10,28 +11,55 @@ import * as _ from 'lodash';
 })
 export class VodPlayerComponent {
 
-  @ViewChild('player', {static: false})
-  public player: ElementRef;
   @Input('vod')
   public vod: Vod;
   @Input('config')
   public config: any = {};
+  public elRef: ElementRef;
+  public videoEl: HTMLVideoElement;
+
+  public constructor(elRef: ElementRef) {
+    this.elRef = elRef;
+  }
 
   public ngAfterViewInit() {
 
-    videojs(this.player.nativeElement, {
-      controls: true,
-      autoplay: true,
-      preload: 'auto',
-      sources: [{
-        src: this.vod.getAttrs().m3u8_url,
-        type: 'application/x-mpegURL'
-      }]
+    this.playAt(0);
+  }
+
+  public playAt(offset: number) {
+
+    if ( $(this.elRef.nativeElement).find('video').length > 0 ) {
+      $(this.elRef.nativeElement).find('video').data('hls').destroy();
+      $(this.elRef.nativeElement).empty();
+    }
+
+    const videoEl = this.elRef.nativeElement.appendChild($('<video class="w-100" controls="true"></video>')[0]);
+
+    $(videoEl).data('hls', new Hls({
+      debug: true,
+      startPosition: offset
+    }));
+    $(videoEl).data('hls').loadSource(this.vod.getAttrs().m3u8_url);
+    $(videoEl).data('hls').attachMedia(videoEl);
+    $(videoEl).data('hls').on(Hls.Events.MEDIA_ATTACHED, () => {
+      videoEl.play();
+    });
+    $(videoEl).data('hls').on(Hls.Events.ERROR, (event, data) => {
+      if ( data.type == Hls.ErrorTypes.MEDIA_ERROR && data.details == 'bufferSeekOverHole' ) {
+        const currentTime = data.reason.match(/seeking from (\d+)/)[1];
+        this.playAt(parseInt(currentTime)+1);
+      }
+    });
+    $(videoEl).on('seeking', () => {
+      if ( offset != parseInt(videoEl.currentTime) ) {
+        this.playAt(parseInt(videoEl.currentTime));
+      }
     });
   }
 
   public seek(offset) {
 
-    videojs(this.player.nativeElement).currentTime(offset);
+    this.playAt(offset);
   }
 }
