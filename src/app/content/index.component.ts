4a0c90186ee8fa9e35f6e925af0436ec7ef66561
jongwoo Yoo <dbwhddn10@gmail.com>
2020-09-23 21:09:34 +0900

    add version history in index page

--- src/app/content/index.component.ts
+++ src/app/content/index.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, SecurityContext, ViewChild } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import marked from 'marked';
 
 @Component({
   selector: 'index-content',
@@ -8,7 +10,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class IndexComponent {
 
-  public constructor(route: ActivatedRoute) {
+  @ViewChild('htmlDiv')
+  public htmlDiv: ElementRef;
+
+  public constructor(private sanitizer: DomSanitizer) {
+
   }
 
+  public ngAfterViewInit() {
+
+    const changelog = require('raw-loader!README.md').default;
+    const markdown = marked(changelog);
+
+    this.htmlDiv.nativeElement.innerHTML = this.sanitizer.sanitize(SecurityContext.HTML, this.sanitizer.bypassSecurityTrustHtml(markdown));
+  }
 }
